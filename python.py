de5ea9439fca148d82590c7e79c26d4b6b1f0b7b
Emma Rivera <emmarivera@ucrwpa4-7-123-46.wnet.ucr.edu>
2018-03-08 18:44:04 -0800

    Official code

--- python.py
+++ python.py
@@ -8,6 +8,7 @@ email = input('What’s your email?')
 phone = input('What is your phone number?')
 title = input('What kind of job are you looking for?')
 title.replace(' ', '%20')
+# prompt user for resume???
 
 page = driver.get('https://www.linkedin.com/jobs/search/?f_LF=f_AL&keywords={}&location=United%20States&locationId=us%3A0'.format(title))
 
@@ -22,8 +23,27 @@ for e in element:
     if '/jobs/view/' in str(e.get_attribute('href')):
         validJobs.append(e.get_attribute('href'))
 
-#test function to print the links. Cindy u can use this loop to traverse 
+def userProfile():
+    useremail = driver.find_element_by_id("apply-form-email-select")
+    userphone = driver.find_element_by_id("apply-form-phone-input")
+    userresume = driver.find_element_by_id("file-browse-input")
+
+    #Imports information into the webiste
+
+    useremail.send_keys(email)
+    userphone.send_keys(phone)
+    userresume.sendKeys("<absolutePathToMyFile>")
+
+    #This will find the button to submit and click the button
+    #Button class jobs-apply-form__submit-button button-primary-large
+
+    driver.find_element_by_xpath('//button[@type="submit"]/span[@class="jobs-apply-form__submit-button button-primary-large"]').click()
+
 for i in validJobs:
     i = 0
-    print(validJobs[i])
+    driver.find_element_by_link_text(validJobs[i]).click() 
+    driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't') 
+    driver.find_element_by_css_selector('div.button.jobs-s-apply_button.js-apply-button ').click()
+    userProfile()
+    driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 'w')
     i = i + 1
